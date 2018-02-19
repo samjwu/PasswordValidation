dd7bf8cd3e2e6fbbe8f089de5a1cc0265dba00a1
unknown <swm1000@hotmail.com>
2018-02-18 22:50:49 -0500

    show err msg for short pwd

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,24 @@
 $("form span").hide();
+
+$("#pwd").keyup(errmsg);
+
+
+/** 
+ * Returns true if pwd is 8 char or over, false otherwise
+*/
+function chkpwdlen() {
+    return $("#pwd").val().length >= 8;
+}
+
+
+/** 
+ * Show error message in span tags if pwd invalid
+*/
+function errmsg() {
+    if(chkpwdlen()) {
+        $(this).next().hide();
+    }
+    else {
+        $(this).next().show();
+    }
+}
